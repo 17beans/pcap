0bc21f68b3d3263f85f81bdb16e90c5432ebfcc1
ChoiKB <gomtangi1@gmail.com>
2017-07-18 04:04:55 +0900

    add session

--- pcap.c
+++ pcap.c
@@ -24,3 +24,21 @@ if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
    net = 0;
    mask = 0;
 }
+/* 세션 열기 */
+handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+if (handle == NULL) {
+   fprintf(stderr, "장치를 열 업습니다.  %s: %s\n", dev, errbuf);
+   return(2);
+}
+/* 필터 컴파일, 적용 */
+if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
+   fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
+   return(2);
+}
+if (pcap_setfilter(handle, &fp) == -1) {
+   fprintf(stderr, "필터를 설치할 수 없습니다.  %s: %s\n", filter_exp, pcap_geterr(handle));
+   return(2);
+
+   }
+
+
